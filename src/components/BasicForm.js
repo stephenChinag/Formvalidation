f9fee2cf66355d3f14fd99ed9afb583f644c3ff8
stephenChing <stephenchinag@gmail.com>
2022-09-20 12:05:47 +0100

    validate

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,3 +1,6 @@
+import React from "react";
+
+
 const BasicForm = (props) => {
   return (
     <form>
--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,9 @@
-import  { useRef, useState} from "react";
+import  {  useState} from "react";
 
 
 const SimpleInput = () => {
 
-const [enteredUsername , setEnteredUserName ]=useState()
+  const [enteredUsername , setEnteredUserName ]=useState('')
 
 
 const nameChangeHandler =(event)=>{
@@ -14,17 +14,29 @@ const nameChangeHandler =(event)=>{
 const formSubmiteHandler =(event)=>{
   event.preventDefault();
 
+  if (enteredUsername.trim()=== ""){
+  console.log('please enter a Value')
+  }
+  else{
   console.log(enteredUsername)
 
+setEnteredUserName('')
+  }
 }
-
 
 
   return (
     <form onSubmit={formSubmiteHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input onChange={nameChangeHandler} type='text' id='name' />
+        <input 
+         
+        onChange={nameChangeHandler} 
+        type='text' 
+        id='name'
+        value={enteredUsername}
+         />
+
       </div>
       <div className="form-actions">
         <button>Submit</button>
